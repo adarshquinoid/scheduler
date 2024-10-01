fd6232a3fdf47db245055a410a2a17b06a6de878
Adarsh A <adarsh.quinoid@gmail.com>
2024-10-01 23:54:27 +0530

    resize and drag and drop

--- src/scheduler/components/Calander/index.tsx
+++ src/scheduler/components/Calander/index.tsx
@@ -1,20 +1,19 @@
 import dayjs from "dayjs";
 import { useState } from "react";
 import { Months, styles } from "../../helpers/constants";
+import EventItem from "../EventItems";
 
-type DatesByMonth = {
-  [key: string]: Date[];
-};
-const Calander = ({ groups = [] }: any) => {
+
+const Calendar = ({ groups = [] }: any) => {
   const [activeYear] = useState(2024);
+  const [dragSample, setDragSample] = useState("");
+  const [newPosition, setNewPostion] = useState("01-01-2024");
 
-  function generateDatesByMonth(): DatesByMonth {
-    const datesByMonth: DatesByMonth = {};
+
+  const generateDatesByMonth = (): any => {
+    const datesByMonth: any = {};
 
     for (let month = 0; month < 12; month++) {
-      //   const monthName = new Date(activeYear, month).toLocaleString("default", {
-      //     month: "long",
-      //   });
       const daysInMonth = new Date(activeYear, month + 1, 0).getDate();
       const dates: Date[] = [];
 
@@ -26,87 +25,95 @@ const Calander = ({ groups = [] }: any) => {
     }
 
     return datesByMonth;
-  }
+  };
 
-  const datesByMonth: DatesByMonth = generateDatesByMonth();
-  
+  const datesByMonth: any = generateDatesByMonth();
+
   const checkForRows = (grp: any) => {
     const isTopLevel = grp.parent === null;
-    //TODO - Make it dynamic with multiple parent support
-    const isTopLevelExpanded=groups?.find(
-      (item: any) => item.parent === null
-    )?.expand;
-    // TODO-END
-    const isParentExpanded = groups?.find(
-      (item: any) => item.id === grp.parent
-    )?.expand;
-    return isTopLevel || (isParentExpanded&&isTopLevelExpanded);
+    const isTopLevelExpanded = groups.find((item:any) => item.parent === null)?.expand;
+    const isParentExpanded = groups.find((item:any) => item.id === grp.parent)?.expand;
+    return isTopLevel || (isParentExpanded && isTopLevelExpanded);
   };
+
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>, date: string) => {
+    setDragSample(date);
+    e.dataTransfer.setData("text/plain", date);
+    e.dataTransfer.effectAllowed = "move"; // Optional: Indicate that the move operation is allowed
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>, day: string) => {
+
+    e.preventDefault();
+    console.log(day)
+    const draggedDate = e.dataTransfer.getData("text/plain");
+    console.log(`Dropped ${draggedDate} on ${day}`);
+    setNewPostion(day)
+
+    // Handle your drop logic here, e.g., updating state, sending data to a server, etc.
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault(); // Necessary to allow dropping
+  };
+
   return (
     <div>
       <div className="flex h-[32px]">
-        {Object.keys(datesByMonth).map((key: any) => (
-          <>
+        {Object.keys(datesByMonth).map(key => (
+          <div
+            key={key}
+            style={{ width: datesByMonth[+key]?.length * styles.dayColWidth }}
+            className="flex text-[12px] leading-[14px] font-normal border-t border-b border-r border-[#EDEAE9]"
+          >
+            <div className="p-2.5">
+              {Months[+key as keyof typeof Months]}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex h-[24px]">
+        {Object.keys(datesByMonth).map(key => (
+          datesByMonth[+key]?.map((day:any) => (
             <div
-              style={{
-                width: datesByMonth[+key]?.length * styles.dayColWidth,
-              }}
-              className="flex text-[12px] leading-[14px] font-normal border-t border-b border-r border-[#EDEAE9]"
+              key={day ? dayjs(day).format("DD-MM-YYYY") : "key"}
+              className="border-b border-r border-[#EDEAE9]"
+              style={{ width: styles.dayColWidth }}
             >
-              <div className="p-2.5 text-[12px] leading-[14px] font-normal h-full">
-                {" "}
-                {Months[+key as keyof typeof Months]}
+              <div className="h-6 w-10 flex items-center justify-center text-[12px] leading-[14px] font-normal">
+                {day ? dayjs(day).format("DD") : ""}
               </div>
             </div>
-          </>
+          ))
         ))}
       </div>
-      <div className="flex h-[24px]">
-        {Object.keys(datesByMonth).map((key: any) => (
-          <>
-            {datesByMonth[+key]?.map((days) => (
-              <div
-                className="border-b border-r border-[#EDEAE9]"
-                style={{ width: styles.dayColWidth }}
-              >
-                <div className="h-6 w-10 flex items-center justify-center text-[12px] leading-[14px] font-normal">
-                  {days ? dayjs(days).format("DD") : ""}
-                </div>
-              </div>
-            ))}
-          </>
-        ))}
-      </div>
+
       <div>
-        <div>
-          {groups?.map((grp: any) => (
-            <>
-              {checkForRows(grp) && (
-                <div className="flex h-[24px]">
-                  {Object.keys(datesByMonth).map((key: any) => (
-                    <>
-                      <>
-                        {datesByMonth[+key]?.map((days) => (
-                          <div
-                            className="border-b border-r border-[#EDEAE9]"
-                            style={{ width: styles.dayColWidth }}
-                          >
-                            <div className="h-6 w-10 flex items-center justify-center text-[12px] leading-[14px] font-normal">
-                              {/* {days ? dayjs(days).format("DD") : ""} */}
-                            </div>
-                          </div>
-                        ))}
-                      </>
-                    </>
-                  ))}
-                </div>
-              )}
-            </>
-          ))}
-        </div>
+        {groups.map((grp:any) => (
+          checkForRows(grp) && (
+            <div key={grp.id} className="flex z-1">
+              {Object.keys(datesByMonth).map(key => (
+                datesByMonth[+key]?.map((day:any) => (
+                  <div
+                    key={day ? dayjs(day).format("DD-MM-YYYY") : "key"}
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, dayjs(day).format("DD-MM-YYYY"))}
+                    className="border-b border-r border-[#EDEAE9] h-10 relative z-1 flex items-center"
+                    style={{ width: styles.dayColWidth }}
+                  >
+                 
+                      <EventItem activeData={day} group={grp} sampleDate={newPosition} handleDragStart={handleDragStart}/>
+                    </div>
+                
+                ))
+              ))}
+            </div>
+          )
+        ))}
       </div>
     </div>
   );
 };
 
-export default Calander;
+export default Calendar;
---
+++ src/scheduler/components/EventItems/index.tsx
@@ -0,0 +1,48 @@
+import dayjs from "dayjs";
+import React, { useEffect, useRef } from "react";
+
+const EventItem: React.FC<any> = ({ activeData, group ,sampleDate,handleDragStart}) => {
+  const resizeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        // Call on resize here
+        console.log(entry.contentRect.width);
+      }
+    });
+
+    if (resizeRef.current) {
+      resizeObserver.observe(resizeRef.current);
+    }
+
+    // Cleanup on unmount
+    return () => {
+      if (resizeRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        resizeObserver.unobserve(resizeRef.current);
+      }
+    };
+  }, []);
+
+  if (
+    group.id === 1 &&
+    dayjs(activeData).format("DD-MM-YYYY") ===  sampleDate
+  ) {
+    return (
+      <div
+        ref={resizeRef}
+        draggable
+        onDragStart={(e) => handleDragStart(e, activeData)}
+        key={activeData}
+        id={activeData}
+        className="absolute  left-0  h-9 rounded-sm overflow-hidden resize-x bg-red-300 z-50 item-resize"
+      >
+        EventItem
+      </div>
+    );
+  }
+  return null;
+};
+
+export default EventItem;
